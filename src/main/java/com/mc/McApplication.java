ed361521553c7b30a29009c1659206a9747d34f1
Sameer Basha <157784893+samlocal@users.noreply.github.com>
2025-05-24 12:56:33 +0530

    Update McApplication.java

--- src/main/java/com/mc/McApplication.java
+++ src/main/java/com/mc/McApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class McApplication {
 
 	public static void main(String[] args) {
+		String name="sam";
 		SpringApplication.run(McApplication.class, args);
 	}
 
